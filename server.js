fb160eb398000b6fe183f7c198540ee3d10715b4
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-03-06 14:51:04 +0000

    getting users method with db

--- server.js
+++ server.js
@@ -86,20 +86,28 @@ app.post('/register', (req, res) => {
             res.json(user[0]);
         })
         .catch(err => res.status(400).json('Unable to register'))
-    })
+})
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json('Not Found');
         }
+        
     })
-    if (!found) {
-        res.status(400).json('no such user');
-    }
+    .catch(err => res.status(400).json('Error getting user'))
+
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+    //         return res.json(user);
+    //     }
+    // })
+  
 })
 
 app.put('/image', (req, res) => {
