01955aa748552aa9bbde85b5e4915cafbf07e507
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-03-06 12:00:34 +0000

    adding a user

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres =require('knex')({
+const db =require('knex')({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
-        user: 'jamie',
+        user: '',
         password: '',
         database: 'smartbrain'
     }
 });
 
-console.log(postgres.select('*').from('users'));
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
+
 
 const app = express();
 
@@ -72,15 +75,12 @@ app.post('/register', (req, res) => {
         console.log(hash);
     });
 
-    database.users.push(
-        {
-            id: 125,
-            name: name,
-            email: email,
-            entries: 0,
-            joined: new Date()
-        }
-    )
+    db('users').insert({
+        email : email,
+        name : name,
+        joined: new Date()
+    }).then(console.log)
+
     res.json(database.users[database.users.length - 1]);
 })
 
