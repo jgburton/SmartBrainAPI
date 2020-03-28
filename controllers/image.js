2454de231f457b5936a8b40b12be541b42c6a9a4
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-03-28 16:08:22 +0000

    Security Update
    Moved API key and logic to backend.

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'd19ff8c77c204b639080fd91e839064f'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = ('/image', (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +24,6 @@ const handleImage = ('/image', (req, res, db) => {
 })
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
